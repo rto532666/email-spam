b39652be401787a8dd0cc98c32be670cdf62fc89
rto532666 <73474905+rto532666@users.noreply.github.com>
2021-02-09 13:05:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template,url_for,request
+import pickle
+import joblib
+
+filename = 'pickle.pkl'
+clf = joblib.load("model.pkl")
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		my_prediction = clf.predict(data)
+	return render_template('index.html',prediction = my_prediction)
+
+if __name__ == '__main__':
+	app.run(debug=True)
